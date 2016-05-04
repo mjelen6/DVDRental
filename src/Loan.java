d42e9773913502212941fac6a064a359998fd659
Maciej Jelen <mjelen@student.agh.edu.pl>
2016-05-04 22:39:44 +0200

    Przyklad uzycia GregorianCallendar

--- src/Loan.java
+++ src/Loan.java
@@ -1,6 +1,4 @@
-import java.sql.Date;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 import org.apache.log4j.BasicConfigurator;
@@ -9,19 +7,17 @@ import org.apache.log4j.Logger;
 public class Loan {
 
 	private static Logger log = Logger.getLogger(Movie.class);
-	private static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm");
-	
+
 	
 	public static void main(String[] args) {
-		
 		
 		BasicConfigurator.configure();
 		log.debug("Application start");
 		
-		GregorianCalendar date = new GregorianCalendar();
-		dateFormat.setTimeZone(date.getTimeZone());
+		GregorianCalendar date = new GregorianCalendar();	
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd kk:mm");
 		
-		System.out.println(dateFormat.format(date.getTime()));
+		System.out.println("" + dateFormat.format(date.getTime()));
 		
 		
 		log.debug("App stop");
